4f60fdb6c1fd8ae2a128f597e6f38b00ddde42d5
jade-r-k <jade.r.s.kelly@gmail.com>
2021-10-15 17:19:32 +0100

    setting up router

---
+++ src/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+      This is the About page!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  components: {
+  }
+}
+</script>
+
+<style>
+.about {
+    text-align: center;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <br>
+      This is the Home page!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
+
+<style>
+.home {
+    text-align: center;
+}
+</style>

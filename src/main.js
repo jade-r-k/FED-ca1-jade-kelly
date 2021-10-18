f0e5f32b12d5116710a3386e4d5757cff2cdf486
jade-r-k <jade.r.s.kelly@gmail.com>
2021-10-18 12:56:46 +0100

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -7,12 +7,11 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue);
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
-
-Vue.use(BootstrapVue);
-Vue.config.productionTip = false
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,13 +2,38 @@
   <div class="home">
       <img alt="Vue logo" src="../assets/logo.png">
       <br>
-      This is the Home page!
+      Search for a country!
+      <br>
+      <input type="text" v-model="term" />
+      <b-button variant="primary" >Search</b-button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+const COUNTRY_URL = "https://restcountries.com/v3.1"
+
 export default {
   name: 'home',
+  data() {
+    return {
+      countries: [],
+      term: "",
+
+    };
+  },
+  mounted() {
+    this.allCountry();
+  },
+  methods: {
+    allCountry() {
+      axios.get(`${COUNTRY_URL}/all`)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch(error => console.log(error))
+    }
+  },
   components: {
   }
 }
